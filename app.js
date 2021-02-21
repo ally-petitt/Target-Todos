d6fab1f92f30843f77a6cf3cce8aaa47c4396d19
virus-spec <allypetitt@gmail.com>
2021-02-21 13:36:25 -0800

    created color theme prottype

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 //add color change to target on hover, but disable event bubbling
+const colorTheme_div = document.querySelector('.color-theme');
+const colorThemes = document.querySelector('.color-theme .hide');
 
+colorTheme_div.addEventListener('mouseover', showThemes);
+colorTheme_div.addEventListener('mouseout', removeThemes);
+
+function showThemes() {
+    colorThemes.classList.remove('hide')
+}
+
+function removeThemes() {
+    colorThemes.classList.add('hide');
+}
 
