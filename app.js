f125dcd4de051001a168c5ebac0f5dd6db4c9e3b
virus-spec <allypetitt@gmail.com>
2021-02-21 19:12:18 -0800

    oh no

--- app.js
+++ app.js
@@ -3,8 +3,14 @@
 const colorTheme_div = document.querySelector('.color-theme');
 const colorThemes = document.querySelector('.color-theme .hide');
 const plusBtn = document.querySelector('.plus-btn');
+const saveBtn = document.querySelector('.save-btn');
 const taskTemplateContainer = document.querySelector('.task-template-container')
+var goal = document.querySelector('.goalEntry');
+var subgoal = document.querySelector('.subgoalEntry');
+var comment = document.querySelector('.commentEntry')
+const targetContainer = document.querySelector('.target-container')
 
+//event listeners
 colorTheme_div.addEventListener('mouseover', showThemes);
 colorTheme_div.addEventListener('mouseout', removeThemes);
 plusBtn.addEventListener('click', () => {
@@ -14,8 +20,11 @@ plusBtn.addEventListener('click', () => {
         showTaskTemplate()
     }
     });
+document.querySelector('.task-template-container form').submit(() => {
+    console.log('submit')
+})
 
-
+//functions
 function showThemes() {
     colorThemes.classList.remove('shrinkingAnimation')
     colorThemes.classList.add('growingAnimation')
@@ -41,4 +50,17 @@ function hideTaskTemplate() {
     taskTemplateContainer.classList.add('template-exit-anim');
     setTimeout(() => {
         taskTemplateContainer.style.display = "none"}, 400)
+}
+
+function createCircle(e) {
+    e.preventDefault();
+    let newCircle = document.createElement('div');
+    newCircle.classList.add('circle');
+    targetContainer.appendChild(newCircle)
+
+    saveInfo(newCircle);
+}
+
+function saveInfo(newCircle) {
+    console.log("")
 }
