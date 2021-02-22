4ddcf7f9c12445a12e3c6ee8d139ce8632358322
virus-spec <allypetitt@gmail.com>
2021-02-21 18:37:21 -0800

    made entry and exit animations for plus button

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 //add color change to target on hover, but disable event bubbling
+//add subtasks when you hover over the task
 const colorTheme_div = document.querySelector('.color-theme');
 const colorThemes = document.querySelector('.color-theme .hide');
+const plusBtn = document.querySelector('.plus-btn');
+const taskTemplateContainer = document.querySelector('.task-template-container')
 
 colorTheme_div.addEventListener('mouseover', showThemes);
 colorTheme_div.addEventListener('mouseout', removeThemes);
+plusBtn.addEventListener('click', () => {
+    if (taskTemplateContainer.style.display == "block") {
+        hideTaskTemplate();
+    } else {
+        showTaskTemplate()
+    }
+    });
+
 
 function showThemes() {
     colorThemes.classList.remove('shrinkingAnimation')
@@ -17,3 +28,17 @@ function removeThemes() {
     colorThemes.classList.remove('growingAnimation')
 }
 
+function showTaskTemplate() {
+    plusBtn.classList.add('rotate-plus-forward');
+    plusBtn.classList.remove('rotate-plus-back');
+    taskTemplateContainer.classList.remove('template-exit-anim');
+    taskTemplateContainer.style.display = "block";
+}
+
+function hideTaskTemplate() {
+    plusBtn.classList.add('rotate-plus-back');
+    plusBtn.classList.remove('rotate-plus-forward');
+    taskTemplateContainer.classList.add('template-exit-anim');
+    setTimeout(() => {
+        taskTemplateContainer.style.display = "none"}, 400)
+}
